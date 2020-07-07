9c5b8751e576bbe072bb6f61f4325d223ce69315
mfnovriansyah <mfnovriansyah@gmail.com>
2020-07-07 13:22:08 +0700

    update database

--- database/health_passport.sql
+++ database/health_passport.sql
@@ -7,11 +7,11 @@ DROP DATABASE IF EXISTS `health_passport`;
 CREATE DATABASE `health_passport`;
 USE `health_passport`;
 
- -- --------------------------------------------------------
-
+-- --------------------------------------------------------
 --
 -- Table structure for table `main_tb`
 --
+DROP TABLE IF EXISTS `main_tb`;
 CREATE TABLE `main_tb` (
   `card_id` varchar(9) NOT NULL PRIMARY KEY,
   `name` text NOT NULL,
@@ -20,59 +20,69 @@ CREATE TABLE `main_tb` (
   `recommended_water_volume` float NOT NULL,
   `drinked_water_volume` float NOT NULL,
   `sick_status` boolean NOT NULL,
-  `temperature` float NOT NULL
+  `class` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `main_tb`
 --
-
-INSERT INTO `main_tb` (`card_id`, `name`, `weight`, `physical_education`, `recommended_water_volume`, `drinked_water_volume`, `sick_status`, `temperature`) VALUES
-('M10802204', 'Mick', 68 , true, 1.5 , 0.6,true, 30.4);
+INSERT INTO `main_tb` (`card_id`, `name`, `weight`, `physical_education`, `recommended_water_volume`, `drinked_water_volume`, `sick_status`, `class`) VALUES
+('M108022041', 'Mick', 68.1, true, 1.5 , 0.6,true, 101);
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `pe_tb`
 --
-
+DROP TABLE IF EXISTS `pe_tb`;
 CREATE TABLE `pe_tb` (
-  `id` varchar(9) NOT NULL PRIMARY KEY,
-  `mon` boolean NOT NULL,
-  `tue` boolean NOT NULL,
-  `wed` boolean NOT NULL,
-  `thu` boolean NOT NULL,
-  `fri` boolean NOT NULL
+  `class_id` int NOT NULL PRIMARY KEY,
+  `mon` varchar(8)  NULL,
+  `tue` varchar(8)  NULL,
+  `wed` varchar(8)  NULL,
+  `thu` varchar(8)  NULL,
+  `fri` varchar(8) NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
 
 --
 -- Dumping data for table `pe_tb`
 --
-
-INSERT INTO `pe_tb` (`id`, `mon`, `tue`, `wed`, `thu`, `fri`) VALUES
-('M10802204',false,false,true,true,false);
-
+INSERT INTO `pe_tb` (`class_id`, `mon`, `tue`, `wed`, `thu`, `fri`) VALUES
+(101,null,null,'13001500',null,null),
+(102,null,'09001100',null,null,null);
 -- --------------------------------------------------------
-
--- --------------------------------------------------------
-
 --
--- Table structure for table `pe_tb`
+-- Table structure for table `sick_tb`
 --
-
-
+DROP TABLE IF EXISTS `sick_tb`;
 CREATE TABLE `sick_tb` (
-  `id` varchar(9) NOT NULL PRIMARY KEY,
-  `mon` boolean NOT NULL,
-  `tue` boolean NOT NULL,
-  `wed` boolean NOT NULL,
-  `thu` boolean NOT NULL,
-  `fri` boolean NOT NULL
+  `card_id` varchar(9) NOT NULL PRIMARY KEY,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- Dumping data for table `sick_tb`
+--
+INSERT INTO `sick_tb` (`card_id`, `date`) VALUES
+('M10802204','2020-07-07'),
+('M10802212','2020-07-02'),
+('M10802215','2020-07-04');
+-- --------------------------------------------------------
+--
+-- Table structure for table `record_tb`
+--
+DROP TABLE IF EXISTS `record_tb`;
+CREATE TABLE `record_tb` (
+  `card_id` varchar(9) NOT NULL PRIMARY KEY,
+  `name` text NOT NULL,
+  `date` datetime NOT NULL,
+  `recommended_water_volume` float NOT NULL,
+  `drinked_water_volume` float NOT NULL,
+  `class` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `record_tb`
+--
+INSERT INTO `record_tb` (`card_id`, `name`, `date`, `recommended_water_volume`, `drinked_water_volume`, `class`) VALUES
+('M10802204', 'Mick', '2020-07-07', 1.5 , 0.6, 101);
 
-INSERT INTO `sick_tb` (`id`, `mon`, `tue`, `wed`, `thu`, `fri`) VALUES
-('M10802204',true,false,false,false,false);
-
+-- --------------------------------------------------------
