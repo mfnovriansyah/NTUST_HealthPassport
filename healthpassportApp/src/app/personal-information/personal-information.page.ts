204bfa31222ac12b8b63deca2bee21417ede32b6
mfnovriansyah <mfnovriansyah@gmail.com>
2020-07-27 17:47:54 +0700

    Adding time to wait for Checking Card ID

--- healthpassportApp/src/app/personal-information/personal-information.page.ts
+++ healthpassportApp/src/app/personal-information/personal-information.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {LoadingController,NavController} from '@ionic/angular';
-import { ActivatedRoute,  Router } from '@angular/router';
+import {LoadingController,NavController, ToastController} from '@ionic/angular';
+import { ActivatedRoute,  Router, NavigationExtras} from '@angular/router';
 
 
 import { ApiService } from '../service/api.service';
+import { ifStmt } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-personal-information',
@@ -14,12 +15,14 @@ export class PersonalInformationPage implements OnInit {
   data : any = []; // for access id
   private deviceid	: '';
   dataForm	: any = {};
+  booleanData : boolean ;
   constructor(
     public navCtrl      : NavController,
     public loadingCtrl	: LoadingController,
     public apiSvc				: ApiService,
     public route	      : ActivatedRoute, 
-		public router	      : Router 
+    public router	      : Router ,
+    private toastCtrl: ToastController,
   ) { 
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) 
@@ -31,7 +34,6 @@ export class PersonalInformationPage implements OnInit {
 
   ngOnInit() {
     this.getData();
-    this.deviceLocked()
   }
 
   //to Logout
@@ -46,7 +48,6 @@ export class PersonalInformationPage implements OnInit {
     this.apiSvc.put('/setlogin/status', this.dataForm).then(
       success => {
         let respon = JSON.parse(this.apiSvc.getDataResult.data);
-        console.log(respon);
         if(respon == "Status updated successfully.")
         {
           if(this.deviceid)
@@ -59,7 +60,14 @@ export class PersonalInformationPage implements OnInit {
         }
         else
         {
-          console.log("failed to update.");
+          console.log("[logout] failed to update.");
+          if(this.deviceid)
+          {
+            this.navCtrl.navigateRoot(['/home', this.deviceid ]);
+          }
+          else{
+            this.navCtrl.navigateRoot('/home');
+          }
         }
       }
     );  
@@ -76,11 +84,11 @@ export class PersonalInformationPage implements OnInit {
         let respon = JSON.parse(this.apiSvc.getDataResult.data);
         if(respon == "Status updated successfully.")
         {
-          console.log("success");
+          console.log("[locked] success");
         }
         else
         {
-          console.log("failed to update.");
+          console.log("[locked] failed to update.");
         }
         
       }
@@ -90,23 +98,70 @@ export class PersonalInformationPage implements OnInit {
   async getData()
   	{
         let loading = await this.loadingCtrl.create({
-            message    : 'Loading Data',
+            message    : 'Checking Card ID <br> Please Put Your Card ID',
+            spinner:    'crescent',
+            cssClass: 'custom-loading',
+            duration:   5000
         });
         await loading.present();
+
+        //to inform if No Card ID Detected
+        let toast;
+        toast = await this.toastCtrl.create({
+          message: 'No Card ID Detected or Invalid ID Card, Please Try Again',
+          duration: 2000,
+          position: 'top'
+        });
         
   		  this.apiSvc.get('/check/device='+this.deviceid+'').then(
   			success => {
-          loading.dismiss();
-          
           let respon = JSON.parse(this.apiSvc.getDataResult.data);
           console.log(respon);
-  				if(respon.Data[0].login ==true && respon.Data[0].locked == false)
-  				{
+          //set timeout and interval for check card id by device id
+          let checkData = setInterval(() => this.checkData(), 1000);
+          setTimeout(() => { clearInterval(checkData); 
+            if(this.booleanData == false){
+              let navigationExtras: NavigationExtras = {
+                state	: {
+                  deviceid	: this.deviceid
+                }
+              };
+              console.log(navigationExtras);
+              
+              if(this.deviceid)
+              {
+                toast.present();
+                this.navCtrl.navigateRoot('/login', navigationExtras);
+                
+              }
+              else{
+                this.navCtrl.navigateRoot('/login');
+              }
+            }
+          }, 5000);
+        }
+  		);
+    }
+
+  //to check Card ID by Device ID 
+  async checkData(){
+    this.apiSvc.get('/check/device='+this.deviceid+'').then(
+      success => {
+        let respon = JSON.parse(this.apiSvc.getDataResult.data);
+          if(!respon.Data[0])
+          {
+            this.booleanData = false;
+          }
+          else if(respon.Data[0].login ==true && respon.Data[0].locked == false)
+          {
+            this.booleanData = true;
+            this.loadingCtrl.dismiss();
             this.data = respon.Data[0];
             //put method to update to database locked
             this.deviceLocked();
-  				}
-  			}
-  		);
-  	}
+          }   
+      }
+    );    
+  }
 }
+
